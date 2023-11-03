ae6011486d143e8e8d04a274e958446ec420229e
lkw <lkw702@naver.com>
2023-11-03 17:38:10 +0900

    first commit

---
+++ src/main/java/com/back/main/MainController.java
@@ -0,0 +1,15 @@
+package com.back.main;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    
+    @GetMapping("/")
+    public String main (){
+
+        return "index.html";
+    }
+    
+}
